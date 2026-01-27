cc7e9b2c4060ebbc10daad31bdae5782c0ba7253
Clemens Beck <cbeck@gitlab.com>
2026-01-27 14:39:33 +0100

    Move Prometheus to OCI

--- scripts/ci/add_dependency_repos.sh
+++ scripts/ci/add_dependency_repos.sh
@@ -2,7 +2,6 @@
 #
 # Add all Helm repos this chart depends on.
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add gitlab https://charts.gitlab.io/
 helm repo add traefik https://helm.traefik.io/traefik
