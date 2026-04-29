5313b8da8ae3a631460d61e03cfc8d20b89bc519
Patrick Cyiza <jpcyiza@gitlab.com>
2026-04-29 09:39:43 +0200

    Add GitLab AI Gateway as subchart
    The AI Gateway is currently considered experimental
    and disabled by default.

--- scripts/ci/add_dependency_repos.sh
+++ scripts/ci/add_dependency_repos.sh
@@ -6,3 +6,4 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add gitlab https://charts.gitlab.io/
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo add haproxy https://haproxytech.github.io/helm-charts
+helm repo add ai-gateway https://gitlab.com/api/v4/projects/gitlab-org%2fcharts%2fai-gateway-helm-chart/packages/helm/devel
