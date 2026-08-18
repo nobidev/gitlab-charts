ca26076fee15ce415344f5763c34e3978c92986e
Marcel van Remmerden <maclbb@hotmail.de>
2026-08-18 13:39:29 +0200

    Require complete APNs settings when any is provided
    The checkConfig previously only fired when authKey.secret was set, so
    keyId or teamId without the secret rendered a clean deploy that never
    delivers. Mirror the omnibus validator: trigger on any of the four
    settings (topic included), then require the auth key secret, key ID,
    and team ID together.

--- spec/integration/check_config/mobile_push_spec.rb
+++ spec/integration/check_config/mobile_push_spec.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'hash_deep_merge'
 
 describe 'checkConfig mobilePush' do
-  describe 'mobilePush.apns' do
+  describe 'mobilePush.apns (key ID and team ID)' do
     let(:success_values) do
       YAML.safe_load(%(
         global:
@@ -34,5 +34,57 @@ describe 'checkConfig mobilePush' do
     include_examples 'config validation',
                      success_description: 'when the APNs auth key, key ID and team ID are provided',
                      error_description: 'when the APNs auth key is provided without key ID and team ID'
+  end
+
+  describe 'mobilePush.apns (auth key secret)' do
+    let(:success_values) do
+      YAML.safe_load(%(
+        global:
+          appConfig:
+            mobilePush:
+              apns:
+                authKey:
+                  secret: gitlab-mobile-push-apns
+                keyId: ABC123DEFG
+                teamId: DEF456GHIJ
+      )).deep_merge!(default_required_values)
+    end
+
+    let(:error_values) do
+      YAML.safe_load(%(
+        global:
+          appConfig:
+            mobilePush:
+              apns:
+                keyId: ABC123DEFG
+                teamId: DEF456GHIJ
+      )).deep_merge!(default_required_values)
+    end
+
+    let(:error_output) { 'When configuring mobile push APNs, be sure to provide the Kubernetes secret' }
+
+    include_examples 'config validation',
+                     success_description: 'when the APNs auth key, key ID and team ID are provided',
+                     error_description: 'when APNs key ID and team ID are provided without the auth key'
+  end
+
+  describe 'mobilePush.apns (topic only)' do
+    let(:success_values) { default_required_values }
+
+    let(:error_values) do
+      YAML.safe_load(%(
+        global:
+          appConfig:
+            mobilePush:
+              apns:
+                topic: com.example.app
+      )).deep_merge!(default_required_values)
+    end
+
+    let(:error_output) { 'When configuring mobile push APNs, be sure to provide the Kubernetes secret' }
+
+    include_examples 'config validation',
+                     success_description: 'when no APNs settings are provided',
+                     error_description: 'when only the APNs topic is provided'
   end
 end
--- templates/_checkConfig_mobilePush.tpl
+++ templates/_checkConfig_mobilePush.tpl
@@ -1,9 +1,15 @@
 {{/*
-Ensure that the mobile push APNs settings are complete when an auth key is provided
+Ensure that the mobile push APNs settings are complete when any of them is provided
 */}}
 {{- define "gitlab.checkConfig.mobilePush.apns" -}}
 {{- with $.Values.global.appConfig.mobilePush.apns }}
-{{-   if .authKey.secret }}
+{{-   if or .authKey.secret .keyId .teamId .topic }}
+{{-     if not .authKey.secret }}
+mobilePush:
+    When configuring mobile push APNs, be sure to provide the Kubernetes secret
+    containing the APNs auth key (`global.appConfig.mobilePush.apns.authKey.secret`).
+    See https://docs.gitlab.com/charts/charts/globals#mobile-push-notification-settings
+{{-     end -}}
 {{-     if not .keyId }}
 mobilePush:
     When configuring mobile push APNs, be sure to specify the key ID of the APNs
